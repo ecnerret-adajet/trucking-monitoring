bd104ad0f21ea7d844336f3ebc621fa3df8469e4
Terrence <tejada.terrence@gmail.com>
2016-12-03 10:49:29 +0800

    Add Email Report Template

--- app/Console/Commands/Report.php
+++ app/Console/Commands/Report.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Mail;
+use App\Track;
+use Carbon\Carbon;
+use DB;
 use Illuminate\Console\Command;
 
 class Report extends Command
@@ -21,14 +24,18 @@ class Report extends Command
      */
     protected $description = 'Send daily email report to admin';
 
+    protected $track;
+
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Track $track)
     {
         parent::__construct();
+
+        $this->track = $track;
     }
 
     /**
@@ -38,7 +45,38 @@ class Report extends Command
      */
     public function handle()
     {
-        $data = array('name'=>"Admin");
+
+        $tracks = Track::latest('created_at')->where('created_at', '>=' ,Carbon::now()->subDays(1))->get();
+       
+       $plantin = Track::latest('created_at')
+                ->where('created_at', '>=' ,Carbon::now()->subDays(1))
+                ->where('entry_plant', '!=', '0000-00-00 00:00:00')
+                ->get();
+
+       $plantout = Track::latest('created_at')
+                ->where('created_at', '>=' ,Carbon::now()->subDays(1))
+                ->where('out_plant', '!=', '0000-00-00 00:00:00')
+                ->get();
+
+       $customerin = Track::latest('created_at')
+                ->where('created_at', '>=' ,Carbon::now()->subDays(1))
+                ->where('in_customer', '!=', '0000-00-00 00:00:00')
+                ->get();   
+
+       $customerout = Track::latest('created_at')
+                ->where('created_at', '>=' ,Carbon::now()->subDays(1))
+                ->where('out_customer', '!=', '0000-00-00 00:00:00')
+                ->get();    
+
+        $data = [
+                'name' => 'Admin',
+                'total_track' => $tracks->count(),
+                'total_plantin' => $plantin->count(),
+                'total_plantout' => $plantout->count(),
+                'total_customerin' => $customerin->count(),
+                'total_customerout' => $customerout->count(),
+        ];
+
          Mail::send('email.report', $data, function($message) {
          $message->to('admin@lafilgroup.com', 'Trucking Monitoring')
                   ->subject('Trucking Monitoring Report');
